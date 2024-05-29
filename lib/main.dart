f8211a3f5867a911ab83f3ef42cd2d2ba73791ae
Lakshya <lakshyakhanna.11152005@gmail.com>
2024-05-29 21:42:15 +0530

    starting the coding part

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask me anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Container(),
+    );
+  }
+}
